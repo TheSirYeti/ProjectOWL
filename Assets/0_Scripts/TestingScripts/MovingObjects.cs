50b64dcc32eff889a5c4abf948774772492a9806
JuanCristofalo <jccristofalo@gmail.com>
2021-09-20 10:08:51 -0300

    Testing

---
+++ Assets/0_Scripts/TestingScripts/MovingObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjects : MonoBehaviour
+{
+  
+    public float  speed;
+    
+    void Update()
+    {
+        transform.position -= transform.forward * speed * Time.deltaTime; 
+        Destroy(gameObject, 5f);
+    }
+}
