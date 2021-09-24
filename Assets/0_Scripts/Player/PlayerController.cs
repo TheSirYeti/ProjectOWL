6c8d4e06a4b4b7cf4ecc3b99462c349bdc99fc8a
JP <jprshaid123@gmail.com>
2021-09-23 21:33:50 -0300

    Start of PlayerController

---
+++ Assets/0_Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Action artificialUpdate;
+    private float minimumSwipeTriggerValue;
+
+    private void Start()
+    {
+        SwipeManager.instance.OnEndTouch += CheckInputs;
+    }
+
+    public void CheckInputs(Vector2 position)
+    {
+        if (position.x >= minimumSwipeTriggerValue)
+        {
+            //transform.position += new Vector3(movementValue, 0, 0);
+        }
+        if (position.x <= (minimumSwipeTriggerValue * -1))
+        {
+            //transform.position += new Vector3(-movementValue, 0, 0);
+        }
+    }
+
+    public void PauseInputs()
+    {
+        
+    }
+}
