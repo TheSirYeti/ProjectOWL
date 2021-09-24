6e0a6818fe7c18903c1d626d10fff9804119aa8d
JP <jprshaid123@gmail.com>
2021-09-23 23:56:39 -0300

    Start of LaneManager and PlayerMovement

---
+++ Assets/0_Scripts/Lanes/LaneManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneManager : MonoBehaviour
+{
+    public int currentLane;
+    public int amountOfLanes;
+    
+    public static LaneManager instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else {Debug.Log("There's already an instance of LaneManager in the scene."); Destroy(gameObject);}
+    }
+
+    public void AddLanes(int amount)
+    {
+        amountOfLanes += amount;
+    }   
+
+    public bool IsLaneChangeAllowed(bool direction)
+    {
+        return default;
+    }
+
+    public void SetCurrentLane(int id)
+    {
+        currentLane = id;
+    }
+}
---
+++ Assets/0_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    void ChangeLane(bool direction)
+    {
+        
+    }
+
+    void Jump()
+    {
+        
+    }
+
+    void Slide()
+    {
+        
+    }
+}
