efb8fbfc93d3251a4c58f09b36c6dfaf20e67484
JP <jprshaid123@gmail.com>
2021-09-28 22:20:19 -0300

    Strategy Start

---
+++ Assets/0_Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Collectible : MonoBehaviour
+{
+    public int value;
+    private void OnTriggerEnter(Collider other)
+    {
+        OnCollect();
+    }
+    public abstract void OnCollect();
+}
---
+++ Assets/0_Scripts/Patrones/Factory/IFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using UnityEngine.UI;
+
+public interface IFactory<T, P> {
+    T Create(P parameter);
+}
+
+public interface IFactory<T> {
+    T Create();
+}
+
+
