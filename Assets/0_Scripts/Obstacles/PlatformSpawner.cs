e58f9ec5daae5968e1fd52425323a20feef9a3d9
JuanCristofalo <jccristofalo@gmail.com>
2021-09-30 15:50:27 -0300

    Enemies spawn correctly

---
+++ Assets/0_Scripts/Obstacles/PlatformSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform[] spawnPoints;
+    ObjectPooler objectPooler;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private float spawnDelay;
+
+    
+   
+    void Start()
+    {   
+        InvokeRepeating("SpawnObstacles", spawnTime, spawnDelay);
+    }
+
+    void SpawnObstacles()
+    {
+        
+        Transform _sp = spawnPoints[ Random.Range (0, spawnPoints.Length)];
+        objectPooler = ObjectPooler.Instance;
+        int rand = Random.Range (0, ObjectPooler.Instance.pools.Count);
+        objectPooler.SpawnFromPool(ObjectPooler.Instance.pools[rand].prefab.tag, _sp.transform.position, _sp.transform.rotation);
+    }
+}
