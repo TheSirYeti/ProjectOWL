7faf7ac5ee5bcf38cdc4fc2b4c2f2157a0dabf28
JP <jprshaid123@gmail.com>
2021-10-01 19:41:19 -0300

    ExtraLife Collectible

---
+++ Assets/0_Scripts/Collectibles/ExtraLife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : Collectible
+{
+    public float speed;
+    public override void OnCollect()
+    {
+        EventManager.Trigger("UpdateScore", value);
+        EventManager.Trigger("SetHP", 1f);
+    }
+    
+    private void FixedUpdate()
+    {
+        StartMoving();
+    }
+
+    public void StartMoving()
+    {
+        transform.position -= transform.forward * Time.deltaTime * speed;
+    }
+}
