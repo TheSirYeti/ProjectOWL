c2e2e03ed22b32776847a0a552e789f552de03f7
JP <jprshaid123@gmail.com>
2021-09-30 21:47:20 -0300

    Looping Roads!

---
+++ Assets/0_Scripts/LevelManager/LoopingFloor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingFloor : MonoBehaviour
+{
+    public Transform startPos, endPos;
+    public float speed;
+
+    private void Update()
+    {
+        transform.position -= Vector3.forward * speed * Time.fixedDeltaTime;
+
+        if (transform.position.z <= endPos.position.z)
+        {
+            transform.position = startPos.position;
+        }
+    }
+}
---
+++ Assets/0_Scripts/LevelManager/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public enum SceneID
+    {
+        MAINMENU,
+        MAINLEVEL
+    }
+
+    public void LoadScene(SceneID scene)
+    {
+        SceneManager.LoadSceneAsync((int) scene);
+    }
+}
