28e091bea02dd2c3be94982e24e4024b8787f3e8
JP <jprshaid123@gmail.com>
2021-10-18 22:36:48 -0300

    Started implementing Object Pool on Main Scene

--- Assets/0_Scripts/Obstacles/Obstacle.cs
+++ Assets/0_Scripts/Obstacles/Obstacle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MovingObjects //, MonoBehaviour
+public class Obstacle : MovingObjects
 {
     public void Start()
     {
