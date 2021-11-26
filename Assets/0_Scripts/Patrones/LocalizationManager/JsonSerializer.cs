4c4b2672c67d67350497760ab182986d5000f3b3
JuanCristofalo <jccristofalo@gmail.com>
2021-11-26 11:46:27 -0300

    Embrace yourselves jackson is coming

---
+++ Assets/0_Scripts/Patrones/LocalizationManager/JsonSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MiniJSON;
+
+public static class JsonSerializer {
+
+    public static void SaveJson<T>(this T obj, string absolutePath) {
+        var json = Json.Serialize(obj);
+        File.WriteAllText(absolutePath, json);
+    }
+
+    public static Dictionary<string, object> LoadJson(string absolutePath) {
+        var text = File.ReadAllText(absolutePath);
+        
+        return (Dictionary<string, object>)Json.Deserialize(text);
+    }
+    
+}
